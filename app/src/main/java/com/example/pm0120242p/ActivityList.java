fdcd09eaf8791b6311444914a65d4fa4a56b2ece
GokuFlash <kevinparedes74@yahoo.com>
2024-06-06 18:48:16 -0600

    Contendio agregado  el dia 03/06/2024

--- app/src/main/java/com/example/pm0120242p/ActivityList.java
+++ app/src/main/java/com/example/pm0120242p/ActivityList.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -17,6 +18,7 @@ public class ActivityList extends AppCompatActivity {
     SQLiteConexion conexion;
     ListView listperson;
     ArrayList<Personas> lista;
+    ArrayList<String> Arreglo;
 
 
 
@@ -29,6 +31,9 @@ public class ActivityList extends AppCompatActivity {
         conexion = new SQLiteConexion(this, Trans.DBname,null, Trans.Version);
         listperson = (ListView) findViewById(R.id.listperson);
         ObtenerInfo();
+
+        ArrayAdapter adp = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Arreglo);
+        listperson.setAdapter(adp);
     }
 
     private void ObtenerInfo()
@@ -52,6 +57,20 @@ public class ActivityList extends AppCompatActivity {
 
         }
 
-
+        cursor.close();
+        FillData();
     }
+
+    private void FillData() {
+
+        Arreglo = new ArrayList<String>();
+        for(int i=0; i<lista.size();i++){
+        Arreglo.add(lista.get(i).getId()+ " "+
+                lista.get(i).getNombres()+ " "+
+                lista.get(i).getApellidos());
+
+
+        }
+    }
+
 }
